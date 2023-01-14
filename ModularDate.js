aa55159735395daeb5b180db9fde29b0169b2345
Break <80194912+Break-Ben@users.noreply.github.com>
2023-01-14 16:09:03 +0800

    Update ModularDate.js
    Added 'shortenned year' option and slightly cleaned code.

--- ModularDate.js
+++ ModularDate.js
@@ -1,6 +1,9 @@
 // Script by Break :) \\
 'use strict';
 
+var dayVar = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+var monthVar = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
 export var scriptProperties = createScriptProperties()
     .addText({
         name: 'format',
@@ -18,36 +21,40 @@ export var scriptProperties = createScriptProperties()
         value: false
     })
     .addCheckbox({
+        name: 'shortennedYear',
+        label: 'Shortenned Year Number',
+        value: false
+    })
+    .addCheckbox({
         name: 'ordinalSuffix',
         label: 'Add Ordinal Suffix (e.g. 3rd not 3)',
         value: false
     })
     .finish();
 
-export function update(value) {
-    var dayVar = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    var monthVar = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+export function update() {
+    let time = new Date();
+    var date = time.getDate()
+    var monthNumber = time.getMonth() + 1
+    var year = time.getFullYear()
+
     if (scriptProperties.shortennedDay) {
         dayVar = dayVar.map(day => day.slice(0, 3));
     }
     if (scriptProperties.shortennedMonth) {
         monthVar = monthVar.map(month => month.slice(0, 3));
     }
-
-    let time = new Date();
-    var day = dayVar[time.getDay()]
-    var date = time.getDate()
+    if (scriptProperties.shortennedYear) {
+        year %= 100
+    }
     if (scriptProperties.ordinalSuffix) {
         date += ['st', 'nd', 'rd'][((date + 90) % 100 - 10) % 10 - 1] || 'th'
     }
-    var monthNumber = time.getMonth() + 1
-    var month = monthVar[monthNumber - 1]
-    var year = time.getFullYear()
 
-    value = scriptProperties.format.replace('{day}', day)
-    value = value.replace('{date}', date)
-    value = value.replace('{month}', month)
-    value = value.replace('{month number}', monthNumber)
-    value = value.replace('{year}', year)
-    return value;
+    return scriptProperties.format
+        .replace('{day}', dayVar[time.getDay()])
+        .replace('{date}', date)
+        .replace('{month}', monthVar[monthNumber - 1])
+        .replace('{month number}', monthNumber)
+        .replace('{year}', year);
 }
